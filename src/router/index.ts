5434958aa0a74daf8a2d647790ff7d48f9dde9bf
Mauro <maurodefalco96@gmail.com>
2025-11-30 23:56:56 +0100

    Dynamic component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
-import HomeView from '@/views/HomeView.vue'
-import ProjectsView from '@/views/ProjectsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +7,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => HomeView,
+      component: HomeView,
     },
     {
       path: '/projects',
       name: 'projects',
-      component: () => ProjectsView,
+      component: import('@/views/ProjectsView.vue'),
     },
   ],
 })
