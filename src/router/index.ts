244a46f3fd072949df49195312fd5a98741f0965
Mauro <maurodefalco96@gmail.com>
2025-12-01 00:25:01 +0100

    Auto Toute Like Nuxt with unplugin vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('h1', { style: 'color: red' }, 'Not found'),
-    },
-    {
-      path: '/projects/:catchAll(.*)*',
-      name: 'projects-not-found',
-      component: h('h1', { style: 'color: red' }, '404 Project Not found'),
-    },
-  ],
+  routes,
 })
 
 export default router
