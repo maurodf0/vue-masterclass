130552d009f91e2cb04457252fbe6cd6ce22b07b
Mauro <maurodefalco96@gmail.com>
2025-12-04 23:45:50 +0100

    feat(projects): implement project fetching and display on projects page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,6 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { onMounted, ref } from 'vue'
+
+const projects = ref([])
+const getProjects = async () => {
+  const { data, error } = await supabase.from('projects').select('*')
+  if (error) console.log(error)
+
+  console.log(data)
+  projects.value = data
+}
+
+onMounted(getProjects)
+</script>
 
 <template>
-  <h1>Projects</h1>
-  <RouterLink to="/">Home</RouterLink>
+  <div>
+    <h1>Projects Page</h1>
+    <p>Welcome to the home page of the Vue Masterclass project.</p>
+    <p>Here you can find a list of all the projects:</p>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <RouterLink :to="{ name: '/projects/[id]', params: { id: project.id } }">
+          {{ project.name }}
+        </RouterLink>
+      </li>
+    </ul>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
+  </div>
 </template>
