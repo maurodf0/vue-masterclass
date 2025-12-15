0eec343199926abf6a8d244011ad2fccd5f71c63
Mauro <maurodefalco96@gmail.com>
2025-12-15 23:49:08 +0100

    Refactors layout components and updates main styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import TopNavigation from '@/components/Layout/TopNavigation.vue'
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
 import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
   <Sidebar />
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavigation />
-    <main>
+    <TopNavbar />
+
+    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+      <div class="flex items-center">
+        <h1 class="text-lg font-semibold md:text-2xl">Page Title</h1>
+      </div>
       <RouterView />
     </main>
   </div>
