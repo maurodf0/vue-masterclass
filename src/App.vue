668498f25d1dc3790bb80006990825f3ebdae7db
Mauro <maurodefalco96@gmail.com>
2025-12-15 23:51:13 +0100

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import TopNavigation from '@/components/Layout/TopNavigation.vue'
 import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
   <Sidebar />
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
+    <TopNavigation />
 
     <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
       <div class="flex items-center">
