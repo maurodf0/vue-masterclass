b4f93fa7bd4736fd0d0da8865a24abd87818074f
Mauro <maurodefalco96@gmail.com>
2025-12-16 00:39:09 +0100

    data table displayed

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, h } from 'vue'
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/DataTable/DataTable.vue'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 const getProjects = async () => {
@@ -13,20 +15,47 @@ const getProjects = async () => {
 }
 
 onMounted(getProjects)
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+  // ...
+]
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+]
 </script>
 
 <template>
-  <div>
-    <h1>Tasks Page</h1>
-    <p>Welcome to the home page of the Vue Masterclass project.</p>
-    <p>Here you can find a list of all the projects:</p>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        <RouterLink :to="{ name: '/projects/[id]', params: { id: task.id } }">
-          {{ task.name }}
-        </RouterLink>
-      </li>
-    </ul>
-    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
-  </div>
+  <DataTable :columns="columns" :data="payments" />
 </template>
