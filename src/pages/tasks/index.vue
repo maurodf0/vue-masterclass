7e3a3693239e67a64bc191e106029c1fec12236f
Mauro <maurodefalco96@gmail.com>
2025-12-29 23:32:06 +0100

    refactor(tasks): replace payments table with tasks table

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -16,46 +16,17 @@ const getProjects = async () => {
 
 onMounted(getProjects)
 
-interface Payment {
-  id: string
-  amount: number
-  status: 'pending' | 'processing' | 'success' | 'failed'
-  email: string
-}
-
-const payments: Payment[] = [
+const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '489e1d42',
-    amount: 125,
-    status: 'processing',
-    email: 'example@gmail.com',
-  },
-  // ...
-]
-
-const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'amount',
-    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      const amount = Number.parseFloat(row.getValue('amount'))
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount)
-
-      return h('div', { class: 'text-right font-medium' }, formatted)
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
     },
   },
 ]
 </script>
 
 <template>
-  <DataTable :columns="columns" :data="payments" />
+  <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
