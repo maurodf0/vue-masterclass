3dc1e16f8f7787899cc82a5876cdf07105cf0d0c
Mauro <maurodefalco96@gmail.com>
2025-12-29 23:51:23 +0100

    projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, h } from 'vue'
+import DataTable from '@/components/ui/dataTable/DataTable.vue'
 
-const projects = ref<any[] | null>(null)
+const projects = ref<Tables<'projects'>[] | null>(null)
 const getProjects = async () => {
   const { data, error } = await supabase.from('projects').select('*')
   if (error) console.log(error)
 
-  console.log(data)
   projects.value = data
 }
 
 onMounted(getProjects)
+
+const columns: columnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    },
+  },
+]
 </script>
 
 <template>
   <div>
-    <h1>Projects Page</h1>
-    <p>Welcome to the home page of the Vue Masterclass project.</p>
-    <p>Here you can find a list of all the projects:</p>
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        <RouterLink :to="{ name: '/projects/[id]', params: { id: project.id } }">
-          {{ project.name }}
-        </RouterLink>
-      </li>
-    </ul>
-    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
+    <DataTable class="mt-4" v-if="projects" :columns="columns" :data="projects" />
   </div>
 </template>
