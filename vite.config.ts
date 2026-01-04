244070ab90c699e31195bda6a8bf7d38c264e8e8
Mauro <maurodefalco96@gmail.com>
2026-01-04 20:50:03 +0100

    unplugin-auto-import install

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     vue({
       template: {
         compilerOptions: {
