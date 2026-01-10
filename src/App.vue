42932968a47eb054021c9c26ed77c20ef30c21f4
Mauro <maurodefalco96@gmail.com>
2026-01-11 00:44:11 +0100

    refactor: improve async task loading and suspense handling

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
 
 <template>
   <AuthLayout>
-    <Suspense>
-      <RouterView />
-    </Suspense>
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" :timeout="0">
+        <Component :is="Component" :key="route.name"> Hi </Component>
+        <template #fallback>
+          <span>Loading...</span>
+        </template>
+      </Suspense>
+    </RouterView>
   </AuthLayout>
 </template>
